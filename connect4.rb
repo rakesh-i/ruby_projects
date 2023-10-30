5992bcb97e201cbb39d289ee6e7f388c1ef8c95e
rakesh-i <rakesh.007.ac@gmail.com>
2023-10-30 21:05:13 +0530

    formated with rubocop

--- connect4.rb
+++ connect4.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class Connect4
   def initialize
     @circle_empty = '○'
     @player1 = '●'
     @player2 = '◼'
-    @grid = Array.new(6){Array.new(7, @circle_empty)}
+    @grid = Array.new(6) { Array.new(7, @circle_empty) }
     @col_fill = Array.new(7, 0)
     system('clear')
     show_grid
@@ -14,39 +16,37 @@ class Connect4
     p1 = '◼'
     p2 = '●'
     flag = 0
-    while count<42
-      if flag==0
-        puts
-        print "Player 1 turn "
+    while count < 42
+      puts
+      if flag.zero?
+        print 'Player 1 turn '
         col = gets.chomp.to_i
         system('clear')
         x = drop(p1, col)
         show_grid
         flag = 1
-        if(x==true)
+        if x == true
           puts "#{p1} has won"
           break
-        elsif(x=="Invalid")
-          puts "Invalid input try again"
+        elsif x == 'Invalid'
+          puts 'Invalid input try again'
           flag = 0
         end
 
       else
-        puts
-        print "Player 2 turn "
+        print 'Player 2 turn '
         col = gets.chomp.to_i
         system('clear')
         x = drop(p2, col)
         show_grid
         flag = 0
-        if x==true
+        if x == true
           puts "#{p2} has won"
           break
-        elsif(x=="Invalid")
-          puts "Invalid input try again"
+        elsif x == 'Invalid'
+          puts 'Invalid input try again'
           flag = 1
         end
-
 
       end
     end
@@ -54,12 +54,12 @@ class Connect4
 
   def show_grid
     puts
-    for i in 0..6
-      print "#{i+1}  "
+    7.times do |i|
+      print "#{i + 1}  "
     end
     puts
-    for i in 0..5
-      for j in 0..6
+    6.times do |i|
+      7.times do |j|
         print "#{@grid[i][j]}  "
       end
       puts
@@ -68,17 +68,14 @@ class Connect4
   end
 
   def drop(player, col)
-    if col>7 || col<=0 || @col_fill[col-1]>=6
-      return "Invalid"
-    else
-      row = 5-@col_fill[col-1]
-      @grid[row][col-1] = player
-      @col_fill[col-1]+=1
-      x = check([6-@col_fill[col-1], col-1])
-      if x==true
-        return true
-      end
-    end
+    return 'Invalid' if col > 7 || col <= 0 || @col_fill[col - 1] >= 6
+
+    row = 5 - @col_fill[col - 1]
+    @grid[row][col - 1] = player
+    @col_fill[col - 1] += 1
+    x = check([6 - @col_fill[col - 1], col - 1])
+    return true if x == true
+
     nil
   end
 
@@ -87,10 +84,9 @@ class Connect4
     dia2 = dia_right_to_left(cur)
     ver = vertical(cur)
     hor = horizontal(cur)
-    if dia1||dia2||ver||hor
-      return true
-    end
-    return false
+    return true if dia1 || dia2 || ver || hor
+
+    false
   end
 
   def dia_left_to_right(cur)
@@ -98,35 +94,29 @@ class Connect4
     col = cur[1]
     player = @grid[row][col]
     count = 0
-    while row<6&&col<7
+    while row < 6 && col < 7
 
-      if @grid[row][col]==player
-        count+=1
-        row+=1
-        col+=1
-        if count==4
-          return true
-        end
-      else
-        break
-      end
+      break unless @grid[row][col] == player
+
+      count += 1
+      row += 1
+      col += 1
+      return true if count == 4
+
     end
-    count-=1
+    count -= 1
     row = cur[0]
     col = cur[1]
-    while row>=0&&col>=0
-      if @grid[row][col]==player
-        count+=1
-        row-=1
-        col-=1
-        if count==4
-          return true
-        end
-      else
-        return false
-      end
+    while row >= 0 && col >= 0
+      return false unless @grid[row][col] == player
+
+      count += 1
+      row -= 1
+      col -= 1
+      return true if count == 4
+
     end
-    return false
+    false
   end
 
   def dia_right_to_left(cur)
@@ -134,35 +124,29 @@ class Connect4
     col = cur[1]
     player = @grid[row][col]
     count = 0
-    while row>=0&&col<7
-      if @grid[row][col]==player
-        count+=1
-        row-=1
-        col+=1
-        if count==4
-          return true
-        end
-      else
-        break
-      end
+    while row >= 0 && col < 7
+      break unless @grid[row][col] == player
+
+      count += 1
+      row -= 1
+      col += 1
+      return true if count == 4
+
     end
-    count-=1
+    count -= 1
     row = cur[0]
     col = cur[1]
 
-    while row<6&&col>=0
-      if @grid[row][col]==player
-        count+=1
-        row+=1
-        col-=1
-        if count==4
-          return true
-        end
-      else
-        return false
-      end
+    while row < 6 && col >= 0
+      return false unless @grid[row][col] == player
+
+      count += 1
+      row += 1
+      col -= 1
+      return true if count == 4
+
     end
-    return false
+    false
   end
 
   def vertical(cur)
@@ -170,31 +154,25 @@ class Connect4
     col = cur[1]
     player = @grid[row][col]
     count = 0
-    while row<6
-      if @grid[row][col]==player
-        count+=1
-        row+=1
-        if count==4
-          return true
-        end
-      else
-        break
-      end
+    while row < 6
+      break unless @grid[row][col] == player
+
+      count += 1
+      row += 1
+      return true if count == 4
+
     end
-    count-=1
+    count -= 1
     row = cur[0]
-    while row>=0
-      if @grid[row][col]==player
-        count+=1
-        row-=1
-        if count==4
-          return true
-        end
-      else
-        return false
-      end
+    while row >= 0
+      return false unless @grid[row][col] == player
+
+      count += 1
+      row -= 1
+      return true if count == 4
+
     end
-    return false
+    false
   end
 
   def horizontal(cur)
@@ -202,35 +180,27 @@ class Connect4
     col = cur[1]
     player = @grid[row][col]
     count = 0
-    while col<7
-      if @grid[row][col]==player
-        count+=1
-        col+=1
-        if count==4
-          return true
-        end
-      else
-        break
-      end
+    while col < 7
+      break unless @grid[row][col] == player
+
+      count += 1
+      col += 1
+      return true if count == 4
+
     end
-    count-=1
+    count -= 1
     col = cur[1]
-    while col>=0
-      if @grid[row][col]==player
-        count+=1
-        col-=1
-        if count==4
-          return true
-        end
-      else
-        return false
-      end
+    while col >= 0
+      return false unless @grid[row][col] == player
+
+      count += 1
+      col -= 1
+      return true if count == 4
+
     end
-    return false
+    false
   end
-
 end
-
 
 game = Connect4.new
 
